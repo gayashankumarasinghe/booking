c8c98e75d7d7fee43479d0ab4be3d2d234dcade8
Gayashan <gayashankaushallya@gmail.com>
2018-07-24 16:12:41 +0530

    Login ok pw 123456 changed firebase authentication email gayashankaushallya@gmail.com

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,78 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { FlashMessagesModule } from 'angular2-flash-messages';
+// AngularFire Imports
+import { AngularFireModule } from 'angularfire2';
+// import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+// Component Imports
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { ClientDetailsComponent } from './components/client-details/client-details.component';
+import { AddclientComponent } from './components/addclient/addclient.component';
+import { EditclientComponent } from './components/editclient/editclient.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+// Service Imports
+import { ClientService } from './services/client.service';
+import { AuthService } from './services/auth.service';
+import { AuthGuard } from './guards/auth.guard';
+
+const appRoutes: Routes = [
+    {path:'', component:DashboardComponent, canActivate:[AuthGuard]},
+    {path:'register', component:RegisterComponent},
+    {path:'login', component:LoginComponent},
+    {path:'addclient',component:AddclientComponent, canActivate:[AuthGuard]}
+];
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyACbaSORORv6SeYdZq-vKOla-QvqX9ZIbE",
+  authDomain: "servicebooking-3a5d8.firebaseapp.com",
+  databaseURL: "https://servicebooking-3a5d8.firebaseio.com",
+  storageBucket: "",
+  messagingSenderId: "552234318066"
+}
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    ClientsComponent,
+    ClientDetailsComponent,
+    AddclientComponent,
+    EditclientComponent,
+    NavbarComponent,
+    SidebarComponent,
+    LoginComponent,
+    RegisterComponent,
+    SettingsComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
+    AngularFireModule.initializeApp(firebaseConfig),
+    FlashMessagesModule
   ],
-  providers: [],
+  providers: [
+    AngularFireAuth,
+    AngularFireDatabase,
+    ClientService,
+    AuthService,
+    AuthGuard
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
